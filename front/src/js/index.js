5332119991e0359a57bb954ef0a9eaf8c6248dbb
lidonghong <lidh@snail.com>
2019-09-26 10:51:03 +0800

    轮播图的鼠标hover事件控制

--- front/src/js/index.js
+++ front/src/js/index.js
@@ -1,20 +1,39 @@
 function Banner() {
-    
+    this.bannerGroup = $("#banner-group");
+    this.index = 0;
+    this.listenBannerHover();
 }
 
-Banner.prototype.run = function(){
+Banner.prototype.listenBannerHover = function () {
+    var self = this;
+  this.bannerGroup.hover(function(){
+      //鼠标移到banner上执行的函数
+      clearInterval(self.timer)
+      },
+      function(){
+      //鼠标从banner移走会执行的函数
+          self.loop()
+      });
+};
+
+Banner.prototype.loop = function(){
+    var self = this;
     var bannerUl = $("#banner-ul");//#+id名称表示选择了id为xxx的控件
-    var index=0;
-    setInterval(function(){
-        if(index >= 3)
+    this.timer = setInterval(function(){
+        if(self.index >= 3)
         {
-            index = 0;
+            self.index = 0;
         }
         else {
-            index++;
+            self.index++;
         }
-        bannerUl.animate({"left":-798*index}, 500);
-    }, 2000)
+        bannerUl.animate({"left":-798*self.index}, 500);
+    }, 2000);
+};
+
+Banner.prototype.run = function(){
+    console.log("abc");
+    this.loop()
 };
 //万能的$方法，确保轮播图片加载完再执行轮播
 $(function(){
