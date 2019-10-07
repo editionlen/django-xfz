c0093c27a076f20f7b6ca22d8d820c3bf3b95b85
lidh <lidh@snail.com>
2019-10-07 17:57:47 +0800

    登录和注册页面切换

--- front/src/js/auth.js
+++ front/src/js/auth.js
@@ -8,3 +8,18 @@ $(function () {
         $(".mask-wrapper").hide()
     })
 });
+
+$(function () {
+    $(".switch").click(function () {
+        var scrollWrapper = $(".scroll-wrapper");
+        var currentLeft = scrollWrapper.css("left");
+        currentLeft = parseInt(currentLeft);
+        if(currentLeft < 0)
+        {
+            scrollWrapper.animate({"left":'0'});
+        }
+        else{
+            scrollWrapper.animate({"left":"-400px"});
+        }
+    });
+});
