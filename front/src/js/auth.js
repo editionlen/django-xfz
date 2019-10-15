a17902921e5ee6367708ccd5745ba73d925d4a34
lidonghong <lidh@snail.com>
2019-10-15 09:35:38 +0800

    后端开发-重构Restful.API的实现

--- front/src/js/auth.js
+++ front/src/js/auth.js
@@ -1,37 +1,15 @@
 
-// //点击登录按钮，弹出模态对话框
-// $(function () {
-//     $("#btn").click(function () {
-//         $(".mask-wrapper").show();
-//     });
-//     $(".close-btn").click(function () {
-//         $(".mask-wrapper").hide()
-//     })
-// });
-
-$(function () {
-    $(".switch").click(function () {
-        var scrollWrapper = $(".scroll-wrapper");
-        var currentLeft = scrollWrapper.css("left");
-        currentLeft = parseInt(currentLeft);
-        if(currentLeft < 0)
-        {
-            scrollWrapper.animate({"left":'0'});
-        }
-        else{
-            scrollWrapper.animate({"left":"-400px"});
-        }
-    });
-});
-
 function Auth() {
     var self=this;
     self.maskWrapper = $('.mask-wrapper');
+    self.scrollWrapper = $('.scroll-wrapper');
 }
 
 Auth.prototype.run = function () {
     var self = this;
     self.listenShowHideEvent();
+    self.listenSwitchEvent();
+    self.listenSigninEvent();
 };
 
 Auth.prototype.showEvent = function () {
@@ -49,19 +27,68 @@ Auth.prototype.listenShowHideEvent = function () {
     var signinBtn = $('.signin-btn');
     var signupBtn = $('.signup-btn');
     var closeBtn = $('.close-btn');
-    var scrollWrapper = $('.scroll-wrapper');
+
     signinBtn.click(function () {
         self.showEvent();
-        scrollWrapper.css({"left":0});
+        self.scrollWrapper.css({"left":0});
     });
     signupBtn.click(function () {
         self.showEvent();
-        scrollWrapper.css({"left":-400});
+        self.scrollWrapper.css({"left":-400});
     });
     closeBtn.click(function () {
         self.hideEvent();
     });
 
+};
+
+Auth.prototype.listenSwitchEvent = function () {
+    var self = this;
+    var switcher = $(".switch");
+    switcher.click(function () {
+        var currentLeft = self.scrollWrapper.css("left");
+        currentLeft = parseInt(currentLeft);
+        if(currentLeft < 0)
+        {
+            self.scrollWrapper.animate({"left":'0'});
+        }
+        else{
+            self.scrollWrapper.animate({"left":"-400px"});
+        }
+    });
+};
+
+Auth.prototype.listenSigninEvent = function () {
+    var signinGroup = $('.signin-group');
+    var telephoneInput = signinGroup.find("input[name='telephone']");
+    var passwordInput = signinGroup.find("input[name='password']");
+    var rememberInput = signinGroup.find("input[name='remember']");
+
+    var submitBtn = signinGroup.find('.submit-btn');
+    submitBtn.click(function () {
+        var telephone = telephoneInput.val();
+        var password = passwordInput.val();
+        var remember = rememberInput.prop("checked");
+
+        xfzajax.post({
+            'url': '/account/login/',
+            'data':{
+                'telephone': telephone,
+                'password': password,
+                'remember': remember?1:0
+            },
+            'success': function (result) {
+                console.log('=============');
+                console.log(result);
+                console.log('=============');
+            },
+            'fail': function (error) {
+                 console.log('xxxxxxxxxxxxxxx');
+                console.log(error);
+                 console.log('xxxxxxxxxxxxxxx');
+            }
+        });
+    })
 };
 
 $(function(){
