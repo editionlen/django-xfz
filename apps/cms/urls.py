81a8110d27aa14b99638ecdbdf562103e9350888
lidh <lidh@snail.com>
2019-10-26 16:11:38 +0800

    后端开发-添加新闻分类前后台功能完成

--- apps/cms/urls.py
+++ apps/cms/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('write_news/', views.WriteNewsView.as_view(), name='write_news'),
     path('news_category/', views.news_category, name='news_category'),
+    path('add_news_category/', views.add_news_category, name='add_news_category'),
 ]
--- apps/cms/views.py
+++ apps/cms/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 from django.views.generic import View
+from django.views.decorators.http import require_POST, require_GET
+from apps.news.models import NewsCategory
+from utils import restful
+
 #页面设计代码从https://adminlte.io/获取
 # Create your views here.
 @staff_member_required(login_url='index')
@@ -11,5 +15,20 @@ class WriteNewsView(View):
     def get(self,request):
         return render(request, 'cms/write_news.html')
 
+@require_GET
 def news_category(request):
-    return render(request, 'cms/news_category.html')
+    categories = NewsCategory.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'cms/news_category.html', context=context)
+
+@require_POST
+def add_news_category(request):
+    name = request.POST.get('name')
+    exists = NewsCategory.objects.filter(name=name).exists()
+    if not exists:
+        NewsCategory.objects.create(name=name)
+        return restful.ok()
+    else:
+        return restful.params_error(message='该分类已经存在！')
