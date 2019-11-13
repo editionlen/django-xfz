710794b9656c211c3a8aa5e26e9c5df3304f6b04
lidh <lidh@snail.com>
2019-11-14 00:06:22 +0800

    后端开发-轮播图管理-轮播图上传功能完成

--- front/src/js/banner.js
+++ front/src/js/banner.js
@@ -4,12 +4,44 @@ function Banners() {
 }
 
 Banners.prototype.listenAddBannerEvent = function () {
-  var addBtn = $("#add-banner-btn");
-  addBtn.click(function () {
+    var self = this;
+    var addBtn = $("#add-banner-btn");
+    addBtn.click(function () {
       var tpl = template("banner-item");
       var bannerListGroup = $(".banner-list-group");
       bannerListGroup.prepend(tpl);
-  });
+
+      var bannerItem = bannerListGroup.find(".banner-item:first");
+      self.addImageSelectEvent(bannerItem);
+    });
+};
+
+Banners.prototype.addImageSelectEvent = function (bannerItem) {
+    var image = bannerItem.find('.thumbnail');
+    var imageInput = bannerItem.find('.image-input');
+    //图片是不能打开文件选择框的，只能通过INput(type=file)
+    image.click(function () {
+       imageInput.click();
+    });
+
+    imageInput.change(function () {
+       var file = this.files[0];
+       var formData = new FormData();
+       formData.append("file", file);
+       xfzajax.post({
+           'url': '/cms/upload_file/',
+           'data':formData,
+           'processData': false,
+           'contentType': false,
+           'success': function (result) {
+               if(result['code'] === 200)
+               {
+                   var url = result['data']['url'];
+                   image.attr('src', url);
+               }
+           }
+       })
+    });
 };
 
 Banners.prototype.run = function () {
